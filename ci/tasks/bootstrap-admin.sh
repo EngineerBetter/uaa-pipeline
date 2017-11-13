ba7fb7671af918cb83663c332e2a0b990a73c812
Colin Simmons <colin.simmons@engineerbetter.com>
2017-11-13 16:06:26 +0000

    bootstrap admin user and move tasks into files

---
+++ ci/tasks/bootstrap-admin.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ "${SKIP_CERT_CHECK}" = "true" ]; then
+  uaac target "https://${APP_NAME}.${CF_APP_DOMAIN}" --skip-ssl-validation
+else
+  uaac target "https://${APP_NAME}.${CF_APP_DOMAIN}"
+fi
+
+uaac client get "$BOOTSTRAP_ADMIN_NAME" || \
+uaac client add "$BOOTSTRAP_ADMIN_NAME" \
+--name "$BOOTSTRAP_ADMIN_NAME" \
+--scope uaa.none \
+--authorized_grant_types client_credentials \
+--authorities uaa.admin \
+--secret "$BOOTSTRAP_ADMIN_SECRET"
---
+++ ci/tasks/test-uaa.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+if [ "${SKIP_CERT_CHECK}" = "true" ]; then
+  uaac target "https://${APP_NAME}.${CF_APP_DOMAIN}" --skip-ssl-validation
+else
+  uaac target "https://${APP_NAME}.${CF_APP_DOMAIN}"
+fi
+
+echo "Check that admin client exists"
+uaac token client get admin -s ${ADMIN_SECRET}
+
+echo "Check that default account doesn't exist"
+set +e
+uaac token owner get cf marissa -s "" -p koala
+result=$?
+set -e
+if [ $result -eq 0 ]; then
+  echo "default user should not exist" && exit 1
+fi
+
+echo "Check that the bootstrapped user exists..."
+uaac user get user -a username
+
+echo "... and can obtain a token..."
+uaac token owner get cf user -s "" -p ${USER_PASSWORD}
+
+echo "... and that the token is valid"
+uaac token decode
