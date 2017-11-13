1241a70f1529416249482a24130a88b124aa8c01
Colin Simmons <colin.simmons@engineerbetter.com>
2017-11-13 16:57:31 +0000

    fix tasks

--- ci/tasks/bootstrap-admin.sh
+++ ci/tasks/bootstrap-admin.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-set -eux
+set -eu
 
 if [ "${SKIP_CERT_CHECK}" = "true" ]; then
   uaac target "https://${APP_NAME}.${CF_APP_DOMAIN}" --skip-ssl-validation
 else
   uaac target "https://${APP_NAME}.${CF_APP_DOMAIN}"
 fi
+
+uaac token client get admin -s ${ADMIN_SECRET}
 
 uaac client get "$BOOTSTRAP_ADMIN_NAME" || \
 uaac client add "$BOOTSTRAP_ADMIN_NAME" \
